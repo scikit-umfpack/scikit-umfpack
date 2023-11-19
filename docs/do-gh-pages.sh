91a443e38c3d83534431fa4ebc04d7c84daba3ae
Robert Cimrman <cimrman3@ntc.zcu.cz>
2023-11-19 19:53:28 +0100

    MAINT: update docs/do-gh-pages.sh

--- docs/do-gh-pages.sh
+++ docs/do-gh-pages.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 set -e -u
-cd "`dirname "$0"`"/..
-rm -rf build
-python setup.py build_sphinx
+cd "`dirname "$0"`" && make clean html && cd ..
 rm -rf build/gh-pages
 mkdir -p build/gh-pages
 git -C build/gh-pages init
 git -C build/gh-pages checkout -b gh-pages
-rsync -a build/sphinx/html/ build/gh-pages/
+rsync -a docs/_build/html/ build/gh-pages/
 touch build/gh-pages/.nojekyll
 git -C build/gh-pages add .
 git -C build/gh-pages commit -m "Generated from sources" -a
